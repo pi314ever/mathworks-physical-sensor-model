a595339698897e537910bf18c97c0edcb3729c9b
pi314ever <piloflyer824@gmail.com>
2022-12-01 11:11:04 -0800

    Small changes and generated results

--- src/point_map_nn/model.py
+++ src/point_map_nn/model.py
@@ -19,38 +19,49 @@ Sequential = tf.keras.models.Sequential
 
 # ------------------------------- CONFIGURATION ------------------------------ #
 
-import argparse
-
-class ModelParserNamespace(argparse.Namespace):
-    train: bool
-    load: bool
-    model_name: str
-    save_plots: bool
-    loss: str
-
-parser = argparse.ArgumentParser(description='Train a neural network to predict point maps')
-parser.add_argument('-t', '--train', action='store_true', help='Train the model')
-parser.add_argument('-l', '--load', action='store_true', help='Load a model from a file')
-parser.add_argument('-n', '--model-name', help='Model name (alphanumeric + _)', type=str, default='point_map_nn')
-parser.add_argument('-s', '--save-plots', help='Saves plots into corresponding pngs', action='store_true')
-parser.add_argument('--loss', help='Loss function to be applied. "mse" or "mae".', default='mse')
-
-args: ModelParserNamespace = parser.parse_args()  # type: ignore # Only adding type annotations
-
+args = None  # type: ignore
 LAYER_SIZES = [16, 16, 16, 16]
 
-LOAD_MODEL = args.load
-TRAIN_MODEL = args.train
+if __name__ == '__main__':
+    import argparse
 
-LOSS = args.loss
+    class ModelParserNamespace(argparse.Namespace):
+        train: bool
+        load: bool
+        model_name: str
+        save_plots: bool
+        loss: str
+        regularization: float
 
-MODEL_FILE = f'{args.model_name}.h5'
+    parser = argparse.ArgumentParser(description='Train a neural network to predict point maps')
+    parser.add_argument('-t', '--train', action='store_true', help='Train the model')
+    parser.add_argument('-l', '--load', action='store_true', help='Load a model from a file')
+    parser.add_argument('-n', '--model-name', help='Model name (alphanumeric + _)', type=str, default='point_map_nn')
+    parser.add_argument('-s', '--save-plots', help='Saves plots into corresponding pngs', action='store_true')
+    parser.add_argument('--loss', help='Loss function to be applied. "mse" or "mae".', default='mse')
+    parser.add_argument('-r', '--regularization', help='L2 regularization constant', type=float, default=0.01)
+
+    args: ModelParserNamespace = parser.parse_args()  # type: ignore # Only adding type annotations
+
+
+    LOAD_MODEL = args.load
+    TRAIN_MODEL = args.train
+
+    LOSS = args.loss
+    REGULARIZATION_CONSTANT = args.regularization
+
+    MODEL_FILE = f'{args.model_name}.h5'
+else:
+    LOSS = 'mse'
+    LOAD_MODEL = False
+    MODEL_FILE = 'point_map_nn.h5'
+    TRAIN_MODEL = True
 
 # ----------------------------- END CONFIGURATION ---------------------------- #
 
 def create_model(layer_sizes=LAYER_SIZES, activation='relu', optimizer='adam', loss=LOSS, metrics=['mse']):
-    layers = [tf.keras.layers.Dense(layer_sizes[0], activation=activation, input_shape=(8,), activity_regularizer=tf.keras.regularizers.l2(0.01))]
-    layers += [tf.keras.layers.Dense(layer_size, activation=activation, activity_regularizer=tf.keras.regularizers.L2(0.01)) for layer_size in layer_sizes[1:]]
+    layers = [tf.keras.layers.Dense(layer_sizes[0], activation=activation, input_shape=(8,), activity_regularizer=tf.keras.regularizers.l2(REGULARIZATION_CONSTANT))]
+    layers += [tf.keras.layers.Dense(layer_size, activation=activation, activity_regularizer=tf.keras.regularizers.L2(REGULARIZATION_CONSTANT)) for layer_size in layer_sizes[1:]]
     model = tf.keras.Sequential(
         layers=layers + [tf.keras.layers.Dense(2, activation='linear')]
     )
@@ -87,6 +98,6 @@ if __name__ == '__main__':
     X_test, Y_test = get_point_map_data('test')
     print(test_model(model, X_test, Y_test))
     Y_pred = model.predict(X_test, batch_size=999999999999)
-    if args.save_plots:
+    if args and args.save_plots:
         visualize.plot_errors(Y_test, Y_pred, title=f'Neural Network Predicted Errors with {args.loss.upper()} Loss', filename=f'errors_histogram_full_{args.model_name}.png')
         visualize.plot_scatter(Y_test, Y_pred, title=f'Neural Network Predicted Sample with {args.loss.upper()} Loss', filename=f'scatter_sample_{args.model_name}.png')
