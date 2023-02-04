42c3bce54755a1917ad2bfac7cf5beadac930b2e
pi314ever <pilotflyer824@gmail.com>
2023-02-03 16:20:03 -0800

    Initial start on images util as wrapper around cv2

--- src/utils/images.py
+++ src/utils/images.py
@@ -1,2 +1,16 @@
 # Util for generating and manipulating image formats
 
+# Image formats are in BGR for convenience to work with cv2 library
+
+import cv2
+
+def read_image(filename: str):
+    """
+    Wrapper around cv2.imread(). Reads an image from a file
+
+    Args:
+        filename (str): Path to image
+    """
+    raw_img = cv2.imread(filename)
+    return raw_img
+    return cv2.cvtColor(raw_img, cv2.COLOR_BGR2RGB)
