1e22b7a2c1338a6a9af1ed4fc94a1120b4368491
pi314ever <pilotflyer824@gmail.com>
2023-05-04 15:31:44 -0700

    Formatted with black

--- src/data/clear_point_maps_data.py
+++ src/data/clear_point_maps_data.py
@@ -7,19 +7,19 @@ from utils._paths import find_data
 from data_util import write_hashmap_data, load_hashmap_data
 
 
-SPLITS = ['test','train','valid']
-MODEL_TYPES = ['radial', 'tangential', 'combined']
+SPLITS = ["test", "train", "valid"]
+MODEL_TYPES = ["radial", "tangential", "combined"]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Delete hash_to_params
     hash_to_params = load_hashmap_data()
-    print(f'Found {len(hash_to_params)} hash_to_params')
-    if input('Delete ALL hash_to_params? (y/N) ') == 'y':
+    print(f"Found {len(hash_to_params)} hash_to_params")
+    if input("Delete ALL hash_to_params? (y/N) ") == "y":
         write_hashmap_data()
-        print('Overwritten hash_to_params.json with {}')
-    if input('Delete ALL point map data? (y/N) ') == 'y':
+        print("Overwritten hash_to_params.json with {}")
+    if input("Delete ALL point map data? (y/N) ") == "y":
         for split in SPLITS:
             for model_type in MODEL_TYPES:
-                for filename in find_data(f'{model_type}_point_maps', split):
-                    print(f'Removing {filename}')
+                for filename in find_data(f"{model_type}_point_maps", split):
+                    print(f"Removing {filename}")
                     os.remove(filename)
