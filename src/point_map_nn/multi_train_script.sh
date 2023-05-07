54194da1440b7285345ef103f7534da26b7d07a4
pi314ever <pilotflyer824@gmail.com>
2023-05-07 02:37:09 -0700

    Reordering multi train script and add bash shebang

--- src/point_map_nn/multi_train_script.sh
+++ src/point_map_nn/multi_train_script.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 # Train models and compare plots
+
+# No regularization
+python model.py combined -r 0
+python model.py radial -r 0
+python model.py tangential -r 0
+
+# Low regularization constant (0.005)
+python model.py combined -r 0.005
+python model.py radial -r 0.005
+python model.py tangential -r 0.005
 
 # Normal regularization constant (0.01)
 python model.py combined -r 0.01
@@ -9,13 +20,3 @@ python model.py tangential -r 0.01
 python model.py combined -r 0.03
 python model.py radial -r 0.03
 python model.py tangential -r 0.03
-
-# Low regularization constant (0.005)
-python model.py combined -r 0.005
-python model.py radial -r 0.005
-python model.py tangential -r 0.005
-
-# No regularization
-python model.py combined -r 0
-python model.py radial -r 0
-python model.py tangential -r 0
